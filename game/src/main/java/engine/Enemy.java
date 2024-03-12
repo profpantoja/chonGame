7bc8868e58632163471ffd9a93263af00092e908
Marcio Gabriel <gabriel09@gmail.com>
2024-03-12 16:20:36 -0300

    update

--- game/src/main/java/engine/Enemy.java
+++ game/src/main/java/engine/Enemy.java
@@ -6,6 +6,6 @@ public class Enemy extends Agent {
 
 	public Enemy(int x, int y) {
 		super(0, 64, 0, 64, x, y, 256, 64, 64);
-		this.setImage(new Image(("file:images/monster.png")));
+		this.setImage(new Image(getClass().getResource("/images/monster.png").toExternalForm()));
 	}
 }
