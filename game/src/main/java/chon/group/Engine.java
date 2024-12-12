1c6df7fdea85d9f631e976dc2ddae90bf67d1e7f
Kadu Pantoja <dsc.pantoja@gmail.com>
2024-12-12 02:01:40 -0300

    Collision methods.

--- game/src/main/java/chon/group/Engine.java
+++ game/src/main/java/chon/group/Engine.java
@@ -75,6 +75,7 @@ public class Engine extends Application {
 					environment.getAgents().get(0).chase(environment.getProtagonist().getPosX(),
 							environment.getProtagonist().getPosY());
 					/* Rendering Objects */
+					environment.detectCollision();
 					environment.drawBackground();
 					environment.drawAgents();
 				}
