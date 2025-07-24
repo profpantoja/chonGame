383f8142a2856bc410233a9888900ea20b454696
Matheus Morelo <mmatheusmmorelo@gmai.com>
2025-07-23 22:54:45 -0300

    sprites inicias adicionados

--- game/src/main/java/chon/group/game/GameSet.java
+++ game/src/main/java/chon/group/game/GameSet.java
@@ -70,35 +70,51 @@ public class GameSet {
                 0,
                 0,
                 canvasHeight,
-                8024,
-                "/images/environment/castleLong.png");
+                3000,
+                "/images/environment/bg1.png");
 
         Level level2 = new Level(
                 0,
                 0,
                 canvasHeight,
-                8000,
-                "/images/environment/mountain.png");
+                3000,
+                "/images/environment/bg2.png");
 
-        environment = new Environment(
+        Level level3 = new Level(
+                0,
+                0,
+                canvasHeight,
+                3000,
+                "/images/environment/bg3.png");
+                
+
+        environment = new Environment(  
                 this.canvasHeight,
                 level1.getWidth(),
                 this.canvasWidth,
                 panel);
 
-        Agent chonBota = new Agent(400, 390, 90, 65, 3, 1000, "/images/agents/chonBota.png", false, false);
-        Weapon cannon = new Cannon(400, 390, 0, 0, 3, 0, 0.05, "", false);
+        Agent darkMage = new Agent(300, 390, 180, 150, 3, 1000, "/images/agents/darkMage.png", false, false);
+        Weapon cannon = new Cannon(400, 460, 0, 0, 3, 0, 0.05, "", false);
         Weapon lancer = new Lancer(400, 390, 0, 0, 3, 0, 0.05, "", false);
 
-        chonBota.setWeapon(cannon);
-        chonBota.setWeapon(lancer);
+        darkMage.setWeapon(cannon);
+        darkMage.setWeapon(lancer);
 
-        Agent chonBot = new Agent(920, 440, 90, 65, 1, 500, "/images/agents/chonBot.png", true, true);
-        environment.setProtagonist(chonBota);
+        Agent chonBot = new Agent(920, 440, 90, 65, 1,  500, "/images/agents/chonBot.png", true, true);
+        environment.setProtagonist(darkMage);
+        environment.setPauseImage("/images/environment/pause.png");
+        environment.setGameOverImage("/images/environment/gameover.png");
+
+        Agent chonBot1 = new Agent(600, 440, 90, 65, 1,  500, "/images/agents/chonBot.png", true, true);
+        environment.setProtagonist(darkMage);
         environment.setPauseImage("/images/environment/pause.png");
         environment.setGameOverImage("/images/environment/gameover.png");
 
         level1.getAgents().add(chonBot);
+        level1.getAgents().add(chonBot1);
+        level2.getAgents().add(chonBot);
+        level2.getAgents().add(chonBot1);
 
         /* Set up some collectable objects */
         List<Object> objects = new ArrayList<>();
@@ -121,6 +137,7 @@ public class GameSet {
         level1.countCollectibles();
         environment.getLevels().add(level1);
         environment.getLevels().add(level2);
+        environment.getLevels().add(level3);
         environment.setCurrentLevel(level1);
     }
 
